755dfcf6475ab90bea694125a2cbb200794cd8df
Cary Robbins <carymrobbins@gmail.com>
2017-04-29 01:50:11 -0500

    Support inheritance

--- src/main/scala/io/estatico/generic/traits/macros/GenericTraitMacros.scala
+++ src/main/scala/io/estatico/generic/traits/macros/GenericTraitMacros.scala
@@ -2,6 +2,7 @@ package io.estatico.generic.traits.macros
 
 import shapeless.{CaseClassMacros, SingletonTypeUtils}
 
+import scala.collection.mutable
 import scala.reflect.macros.whitebox
 
 @macrocompat.bundle
@@ -10,7 +11,7 @@ private[traits] final class GenericTraitMacros(val c: whitebox.Context)
 
   import c.universe._
 
-  def materializeGenericTrait[T : WeakTypeTag, R: WeakTypeTag]: Tree = {
+  def materializeGenericTrait[T : WeakTypeTag, R : WeakTypeTag]: Tree = {
     val typ = weakTypeOf[T]
     if (!typ.typeSymbol.isAbstract) {
       c.abort(c.enclosingPosition, "Cannot materialize non-abstract class")
@@ -21,16 +22,14 @@ private[traits] final class GenericTraitMacros(val c: whitebox.Context)
     val toBody = makeHListVal(fields.map(_._1))
 
     q"""
-      {
-        new $GenericClass[$typeName] {
+      new $GenericClass[$typeName] {
 
-          override type Repr = $ReprType
+        override type Repr = $ReprType
 
-          override def to(t: $typeName): Repr = $toBody
+        override def to(t: $typeName): Repr = $toBody
 
-          override def from(r: Repr): $typeName = new $typeName {
-            ..${makeOverrides(fields)}
-          }
+        override def from(r: Repr): $typeName = new $typeName {
+          ..${makeOverrides(fields)}
         }
       }: $GenericObj.Aux[$typeName, $ReprType]
     """
@@ -47,8 +46,8 @@ private[traits] final class GenericTraitMacros(val c: whitebox.Context)
     val labelValues = labels.map(mkSingletonSymbol)
 
     val labelsType = mkHListTpe(labelTypes)
-    val labelsValue = labelValues.foldRight(q"$HNilObj")((x, acc) =>
-      q"$HConsObj($x, $acc)"
+    val labelsValue = labelValues.foldRight(q"$HNilObj")(
+      (x, acc) => q"$HConsObj($x, $acc)"
     )
 
     q"""
@@ -60,14 +59,25 @@ private[traits] final class GenericTraitMacros(val c: whitebox.Context)
   }
 
   private def collectFields(typ: Type): List[(TermName, Type)] = {
-    typ.members.sorted.collect { case m if m.isAbstract && m.typeSignature.paramLists == Nil =>
-      (m.name.toTermName, m.typeSignature.resultType)
+    // Retain order of fields as they are collected.
+    val fields = new mutable.LinkedHashMap[TermName, Type]
+    typ.baseClasses.reverse.foreach { cls =>
+      cls.asType.toType.members.sorted.foreach { s =>
+        val isEligible = (
+          s.isAbstract
+          && s.isMethod
+          && s.asMethod.paramLists == Nil
+          && s.asMethod.typeParams == Nil
+        )
+        if (isEligible) fields += (s.name.toTermName -> s.typeSignature.resultType)
+      }
     }
+    fields.toList
   }
 
   private def makeOverrides(fields: List[(TermName, Type)]): List[Tree] = {
     fields.zipWithIndex.map { case ((n, t), i) =>
-      ValDef(Modifiers(Flag.OVERRIDE), n, tq"$t", q"r($i)")
+      DefDef(Modifiers(), n, Nil, Nil, tq"$t", q"r($i)")
     }
   }
 
@@ -81,25 +91,14 @@ private[traits] final class GenericTraitMacros(val c: whitebox.Context)
     case n :: ns => q"$HConsObj(t.$n, ${makeHListVal(ns)})"
   }
 
-  private def makeCaseClass(typ: Type): (TypeName, Tree) = {
-    val name = TypeName(
-      typ.typeSymbol.name.decodedName.toString + "Impl"
-    )
-    val caseFields = collectFields(typ).map { case (n, t) =>
-      ValDef(Modifiers(Flag.CASEACCESSOR | Flag.PARAMACCESSOR), n, tq"$t", EmptyTree)
-    }
-    (name, q"case class $name(..$caseFields) extends $typ")
-  }
-
-  private val DefaultSymbolicLabellingClass = getType[shapeless.DefaultSymbolicLabelling[_]]
+  private val DefaultSymbolicLabellingClass = typeSym[shapeless.DefaultSymbolicLabelling[_]]
   private val DefaultSymbolicLabellingObj = DefaultSymbolicLabellingClass.companion
-  private val GenericClass = getType[shapeless.Generic[_]]
+  private val GenericClass = typeSym[shapeless.Generic[_]]
   private val GenericObj = GenericClass.companion
-  private val HConsClass = getType[shapeless.::[_, _]]
+  private val HConsClass = typeSym[shapeless.::[_, _]]
   private val HConsObj = HConsClass.companion
-  private val HNilClass = getType[shapeless.HNil]
+  private val HNilClass = typeSym[shapeless.HNil]
   private val HNilObj = HNilClass.companion
 
-  // TODO: Is the .typeSymbol.asType necessary?
-  private def getType[T : TypeTag] = typeOf[T].typeSymbol.asType
+  private def typeSym[A : TypeTag] = typeOf[A].typeSymbol.asType
 }
